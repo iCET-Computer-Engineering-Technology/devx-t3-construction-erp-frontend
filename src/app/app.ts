92a3b27028a44440d45d037102ec328a9a43d9b5
Malindu Peiris <malindupeiris5@gmail.com>
2026-03-25 14:41:08 +0530

    Updated user roles side bars for user login

--- src/app/app.ts
+++ src/app/app.ts
@@ -44,22 +44,35 @@ export class App {
   // };
 
   private readonly ALL_MAIN_NAV = [
-    { label: 'Dashboard', icon: 'dashboard', route: '/dashboard', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'WORKER','ACCOUNTANT'] },
-    { label: 'Projects', icon: 'folder_open', route: '/projects', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'WORKER'] },
-    { label: 'Tasks', icon: 'task_alt', route: '/tasks', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'WORKER'] },
-    { label: 'WBS', icon: 'account_tree', route: '/wbs', roles: ['ADMIN', 'PROJECT_MANAGER'] },
-    { label: 'Progress', icon: 'trending_up', route: '/progress', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
-    { label: 'Equipment', icon: 'construction', route: '/equipment', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
-    { label: 'Workforce', icon: 'groups', route: '/workforce', roles: ['ADMIN', 'PROJECT_MANAGER'] },
-    { label: 'Budget', icon: 'account_balance_wallet', route: '/budget', roles: ['ACCOUNTANT'] },
-    { label: 'Documents', icon: 'description', route: '/documents', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
-    { label: 'Reports', icon: 'bar_chart', route: '/reports', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
-    { label: 'Users', icon: 'manage_accounts', route: '/users', roles: ['ADMIN'] },
+    { label: 'Dashboard', icon: 'dashboard', route: '/dashboard', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'ACCOUNTANT', 'WORKER'] },
+    { label: 'My Tasks', icon: 'assignments', route: '/my-tasks',   roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'WORKER'] },
+    { label: 'Projects', icon: 'folder_open', route: '/projects', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'ACCOUNTANT'] },
+    { label: 'Tasks', icon: 'task_alt', route: '/tasks',          roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
+    { label: 'WBS', icon: 'account_tree', route: '/wbs',          roles: ['ADMIN', 'PROJECT_MANAGER'] },
+    { label: 'Progress', icon: 'timeline', route: '/progress',   roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'WORKER'] },
+    { label: 'Equipment', icon: 'construction', route: '/equipmemt', roles: ['ADMIN', 'SITE_ENGINEER'] },
+    { label: 'Workforce', icon: 'groups', route: '/workforce',  roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
+    { label: 'Budget', icon: 'account_balance_wallet', route: '/budget',          roles: ['ADMIN', 'PROJECT_MANAGER', 'ACCOUNTANT'] },
+    { label: 'Documents', icon: 'description', route: '/documents',  roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'ACCOUNTANT'] },
+    { label: 'Reports', icon: 'assessment', route: '/reports',        roles: ['ADMIN', 'PROJECT_MANAGER', 'ACCOUNTANT'] },
+    { label: 'Users', icon: 'manage_accounts', route: '/users',              roles: ['ADMIN', ] }, 
+
+
+    // { label: 'Projects', icon: 'folder_open', route: '/projects', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'WORKER'] },
+    // { label: 'Tasks', icon: 'task_alt', route: '/tasks', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'WORKER'] },
+    // { label: 'WBS', icon: 'account_tree', route: '/wbs', roles: ['ADMIN', 'PROJECT_MANAGER'] },
+    // { label: 'Progress', icon: 'trending_up', route: '/progress', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
+    // { label: 'Equipment', icon: 'construction', route: '/equipment', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
+    // { label: 'Workforce', icon: 'groups', route: '/workforce', roles: ['ADMIN', 'PROJECT_MANAGER'] },
+    // { label: 'Budget', icon: 'account_balance_wallet', route: '/budget', roles: ['ACCOUNTANT'] },
+    // { label: 'Documents', icon: 'description', route: '/documents', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
+    // { label: 'Reports', icon: 'bar_chart', route: '/reports', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER'] },
+    // { label: 'Users', icon: 'manage_accounts', route: '/users', roles: ['ADMIN'] },
   ];
 
   private readonly ALL_BOTTOM_NAV = [
     { label: 'RBAC', icon: 'admin_panel_settings', route: '/rbac', roles: ['ADMIN'] },
-    { label: 'Settings', icon: 'settings', route: '/settings', roles: ['ADMIN','ACCOUNTANT','PROJECT_MANAGER','WORKER','SITE_ENGINEER'] },
+    { label: 'Settings', icon: 'settings', route: '/settings', roles: ['ADMIN', 'PROJECT_MANAGER', 'SITE_ENGINEER', 'ACCOUNTANT', 'WORKER'] },
   ];
 
   // protected currentUser: TopBarUser = {
